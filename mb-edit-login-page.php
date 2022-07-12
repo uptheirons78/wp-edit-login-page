061bad1ab5f714e8556047884eca7b609a490ede
mauro bono <bono.mauro@gmail.com>
2022-07-12 17:06:27 +0200

    [update] styles using css variables

--- mb-edit-login-page.php
+++ mb-edit-login-page.php
@@ -27,6 +27,12 @@ define('TICKER', WPMU_PLUGIN_URL . ASSETS_DIR . TICKER_FILE);
 function mb_login_style()
 { ?>
   <style type="text/css">
+    /* Admin CSS Variables */
+    :root {
+      --mb-login-clr-primary: #c11718;
+      --mb-login-clr-primary-clearer: rgba(193, 23, 24, .8);
+    }
+
     /* Background styles */
     .login {
       min-height: 100%;
@@ -44,25 +50,36 @@ function mb_login_style()
       background-repeat: no-repeat;
     }
 
+    /* A tag focus */
+    .login a:focus {
+      box-shadow: 0 0 0 1px var(--mb-login-clr-primary), 0 0 2px 1px var(--mb-login-clr-primary-clearer) !important;
+    }
+
+    /* Button and Button Primary focus */
+    .login .button-primary:focus,
+    .login .button:focus {
+      box-shadow: 0 0 0 1px #fff, 0 0 0 3px var(--mb-login-clr-primary) !important;
+    }
+
     /* Inputs styles: username, password and remember-me inputs */
     .login input[type=text]:focus,
     .login input[type=password]:focus,
     .login input[type=checkbox]:focus {
-      border-color: #c11718;
-      box-shadow: 0 0 0 1px #c11718;
+      border-color: var(--mb-login-clr-primary);
+      box-shadow: 0 0 0 1px var(--mb-login-clr-primary);
       outline: 2px solid transparent;
     }
 
     /* Icon inside password input styles */
     .login .dashicons-visibility,
     .login .dashicons-hidden {
-      color: #c11718;
+      color: var(--mb-login-clr-primary);
     }
 
     /* Focus styles for hide password button */
     .login .button.wp-hide-pw:focus {
-      border-color: #c11718 !important;
-      box-shadow: 0 0 0 1px #c11718 !important;
+      border-color: var(--mb-login-clr-primary) !important;
+      box-shadow: 0 0 0 1px var(--mb-login-clr-primary) !important;
       outline: 2px solid transparent;
     }
 
@@ -73,44 +90,69 @@ function mb_login_style()
 
     /* #nav and #backtoblog link styles on hover */
     .login #nav a:hover,
-    .login #backtoblog a:hover {
-      color: #c11718 !important;
+    .login #nav a:focus,
+    .login #backtoblog a:hover,
+    .login #backtoblog a:focus {
+      color: var(--mb-login-clr-primary) !important;
+      box-shadow: 0 0 0 1px var(--mb-login-clr-primary), 0 0 2px 1px var(--mb-login-clr-primary-clearer) !important;
     }
 
     /* Submit button styles */
     .login .button-primary {
-      background: #c11718 !important;
-      border-color: #c11718 !important;
+      background: var(--mb-login-clr-primary) !important;
+      border-color: var(--mb-login-clr-primary) !important;
     }
 
     /* Privacy Policy link styles */
     .login .privacy-policy-link {
-      color: #c11718;
+      color: var(--mb-login-clr-primary);
     }
 
     /* Language switcher styles: on focus */
     .login .language-switcher select:focus {
-      border-color: #c11718;
-      color: #c11718;
-      box-shadow: 0 0 0 1px #c11718;
+      border-color: var(--mb-login-clr-primary);
+      color: var(--mb-login-clr-primary);
+      box-shadow: 0 0 0 1px var(--mb-login-clr-primary);
     }
 
     /* Language switcher styles: on hover */
     .login .language-switcher select:hover {
-      color: #c11718;
+      color: var(--mb-login-clr-primary);
+    }
+
+    /* Language switcher styles: on focus */
+    .login .language-switcher select:focus {
+      border-color: var(--mb-login-clr-primary) !important;
+      color: var(--mb-login-clr-primary) !important;
+      box-shadow: 0 0 0 1px var(--mb-login-clr-primary) !important;
+    }
+
+    /* Reset Firefox inner outline that appears on :focus. */
+    /* This ruleset overrides the color change on :focus thus needs to be after select:focus. */
+    .login .language-switcher select:-moz-focusring {
+      text-shadow: 0 0 0 var(--mb-login-clr-primary) !important;
+    }
+
+    /* Remove background focus style from IE11 while keeping focus style available on option elements. */
+    .login .language-switcher select:hover::-ms-value {
+      color: var(--mb-login-clr-primary) !important;
+    }
+
+    .login .language-switcher select:focus::-ms-value {
+      color: var(--mb-login-clr-primary) !important;
     }
 
     /* Language switcher styles: submit button */
     .login .language-switcher input[type=submit] {
-      color: #c11718;
-      border-color: #c11718;
+      color: var(--mb-login-clr-primary);
+      border-color: var(--mb-login-clr-primary);
       background: #f6f7f7;
       vertical-align: top;
     }
 
     /* Loged out message styles */
     .login .message {
-      border-left: 4px solid #c11718 !important;
+      border-left: 4px solid var(--mb-login-clr-primary) !important;
     }
   </style>
 <?php }
